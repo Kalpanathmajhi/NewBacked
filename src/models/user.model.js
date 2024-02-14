fb9af6f987d8bc41776d14da6ec28ade68d46408
Kalpanathmajhi <71187581+Kalpanathmajhi@users.noreply.github.com>
2024-02-15 00:49:11 +0530

    added bcrypt and jwt

---
+++ src/models/user.model.js
@@ -0,0 +1,73 @@
+import mongoose, {Schema} from "mongoose";
+import jwt  from 'jsonwebtoken';
+import bcrypt  from 'bcrypt'
+
+const userSchema = new Schema({
+
+
+    usename : {
+        type: String,
+        required: true,
+        uniquie: true,
+        lowercase :true,
+        trim: true,
+        index: true,
+    },
+    email : {
+        type: String,
+        required: true,
+        uniquie: true,
+        lowercase :true,
+        trim: true,
+       
+    },
+    fullName : {
+        type: String,
+        required: true,
+        trim: true,
+        index:true,
+    },
+    avatars : {
+        type: String, //cloudnary  image url 
+        required: true,
+    },
+    coverImage : {
+        type: String,
+        required: true,
+    },
+    watchHistory : {
+        type: Schema.Types.ObjectId,
+        ref: "Video"
+    },
+    password : {
+        type: String,
+        required: true,
+        uniquie: [true, 'Password is required'],
+      
+       
+    },
+    refreshToken : {
+        type: String,
+       
+       
+    },
+   
+},
+{timestamps: true}
+)
+
+
+userSchema.pre("save",async function (next){
+    if(!this.isModified('password')){
+      return next();
+    }
+    this.password = bcrypt.hash(this.password, 10)
+    next()
+})
+
+userSchema.methods.isPaawordCorrect = async function
+(password){
+return await  bcrypt.compare(password , this.password)
+}
+
+export const User = mongoose.model("User", userSchema)
---
+++ src/utils/ApiResponse.js
@@ -0,0 +1,8 @@
+class ApiResponse {
+    constructor (statusCode, date, message = "Sucess"){
+        this.statusCode = statusCode;
+        this.date = date;
+        this.message = message;
+        this.success = statusCode < 400
+    }
+}
