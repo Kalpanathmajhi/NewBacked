12d8aafd0718947c2879d65557b9e7d933074820
Kalpanathmajhi <71187581+Kalpanathmajhi@users.noreply.github.com>
2024-02-22 23:45:24 +0530

    added jwt

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -70,4 +70,32 @@ userSchema.methods.isPaawordCorrect = async function
 return await  bcrypt.compare(password , this.password)
 }
 
+userSchema.methods.generateAccessToken = function(){
+
+ return  jwt.sign({
+    _id: this._id,
+    email : this.email,
+    usename:this.usename,
+    fullName: this.fullName
+},
+process.env.ACCESS_TOKEN_SECRET,
+{
+    expiresIn : process.env.ACCESS_TOKEN_EXPIRY
+}
+)
+}
+userSchema.methods.generateRefreshToken = function(){
+    return  jwt.sign({
+        _id: this._id,
+
+    },
+    process.env.REFRESH_TOKEN_SECRET,
+    {
+        expiresIn : process.env.REFRESH_TOKEN_EXPIRY
+    }
+    )  
+}
+
+
+
 export const User = mongoose.model("User", userSchema)
