18b5a9340a4886817d9e3c2264e0f60bdec2ebbc
Kalpanathmajhi <71187581+Kalpanathmajhi@users.noreply.github.com>
2024-02-28 23:40:16 +0530

    added validation

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,9 +1,28 @@
 import { asyncHandler } from "../utils/asyncHandler.js"
+import { ApiError} from "../utils/ApiError.js"
+import {User} from  '../models/user.model.js'
 const registerUser = asyncHandler (async (req,res)=> {
-  res.status(200).json({
-        message: "ok"
-    })
-   
-} )
+ //get user details from frontend
+ //validation - not empty
+ // check if user already exists:userName, email
+ //check for images and check for avatar
+ //upload in cloudnary
+  //create user object - crrate entry in db
+  //remove password and response token fiels from response
+  //return res 
+ const {fullName, email, username, password} = req.body
+ console.log("email",email);
+if (
+  [fullName, email, username, password].some((field)=> 
+  field?.trim() === "")
+
+){
+throw new ApiError(400, "All fields are required")
+}
+const existedUser=User.findOne( {$or:[{username},{email}]})
+})
+if (existedUser) {
+  throw new ApiError(409,"User already exist");
+}
 
 export {registerUser}
