6a28acaae1a42c5aeb26374ab28ff9e978f0b6cc
Kalpanathmajhi <71187581+Kalpanathmajhi@users.noreply.github.com>
2024-03-04 22:21:31 +0530

    added logout

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -137,11 +137,32 @@ const loginUser = asyncHandler(async (req, res) => {
 
 })
 const logoutUser = asyncHandler(async (req,res)=>{
+ await User.findByIdAndUpdate(
+    req.user.id,
+    {
+      $set: {
+        refreshToken:undefined
+      }
+    },
+    {
+      new:true
+    }
+  )
+  const options ={
+    httpOnly :true,
+    secure:true
+  }
+  return  res.status(200)
+  .clearCookie('acessToken',options)
+  .clearCookie('refreshToken', options)
+  .json(new ApiResponse(200, {}, "User logged out"))
   
+
 })
 
 
 export {
   registerUser,
   loginUser,
+  logoutUser
 }
